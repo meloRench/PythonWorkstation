3cd5c500f1bfab9897aa8693a86424952ee1f72a
meloRench <melo.rench@gmail.com>
2016-04-10 18:12:35 +0800

    模板和静态媒体

--- melo_tango/rango/views.py
+++ melo_tango/rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+	context_dict = { 'boldmessage': "viva la vida"}
+	return render(request,'rango/index.html',context_dict)
+   # return HttpResponse("Rango says hey there world! <br/> <a href='/rango/rc'>About</a>")
+
+def  about(request):
+	return HttpResponse("about Page!<br/> <a href='/rango/'>Index</a>")
