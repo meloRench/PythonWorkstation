260cfea834b2dcc8f2ed03cb5b0cca6697701da2
meloRench <melo.rench@gmail.com>
2016-04-14 23:50:15 +0800

    URL设计与映射

--- melo_tango/rango/views.py
+++ melo_tango/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
 	Category_list = Category.objects.order_by('-views')[:5]
@@ -12,6 +13,23 @@ def index(request):
 
 	return render(request,'rango/index.html',context_dict)
    # return HttpResponse("Rango says hey there world! <br/> <a href='/rango/rc'>About</a>")
+
+def category(request):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name']= category.name
+
+		pages = Page.objects.filter(category=category)
+
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+
+	return render(request,'rango/category.html',context_dict)
+
 
 def  about(request):
 	return HttpResponse("about Page!<br/> <a href='/rango/'>Index</a>")
