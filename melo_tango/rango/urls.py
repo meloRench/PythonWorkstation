2fa935a29f0c9273d0068849bb5753c5e76e7b4d
meloRench <melo.rench@gmail.com>
2016-04-17 23:01:03 +0800

    注册视图与模板

--- melo_tango/rango/urls.py
+++ melo_tango/rango/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^about/$', views.about, name='about'),
     url(r'^rc/',views.about,name='rc'),
     url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^register/$', views.register, name='register'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),) 
--- melo_tango/rango/views.py
+++ melo_tango/rango/views.py
@@ -45,5 +45,45 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+from rango.forms import UserForm, UserProfileForm
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+       
+       
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+       
+        if user_form.is_valid() and profile_form.is_valid():
+       
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+       
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+            'rango/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
+
 def  about(request):
 	return HttpResponse("about Page!<br/> <a href='/rango/'>Index</a>")
