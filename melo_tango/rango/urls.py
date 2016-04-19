0971b5ed0520bc7ef0f73b5818ac2dc65ae20a01
meloRench <melo.rench@gmail.com>
2016-04-19 23:07:43 +0800

    login

--- melo_tango/rango/urls.py
+++ melo_tango/rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^rc/',views.about,name='rc'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),) 
